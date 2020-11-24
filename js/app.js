311e20c3d1c05823b5ca39c6633352a23286c56c
leandro <dleo86@gmail.com>
2020-11-24 11:24:54 -0300

    Se agregan mejoras: Paginación en javascript y en css

--- js/app.js
+++ js/app.js
@@ -3,6 +3,11 @@ const carrito = document.querySelector('#carrito'); //linea 23
 const listaCursos = document.querySelector('#lista-cursos');//linea 85
 const contenedorCarrito = document.querySelector('#lista-carrito tbody');//linea 35
 const vaciarCarritoBtn = document.querySelector('#vaciar-carrito'); //linea 38
+const pag = document.querySelectorAll('.pag');
+const row1 = document.querySelector("#row1");
+const row2 = document.querySelector("#row2");
+const row3 = document.querySelector("#row3");
+const row4 = document.querySelector("#row4");
 let articulosCarrito = [];
 
 // Listeners
@@ -17,6 +22,62 @@ function cargarEventListeners() {
 
      // Al Vaciar el carrito
      vaciarCarritoBtn.addEventListener('click', vaciarCarrito);
+
+     row2.style.display = "none";
+     row3.style.display = "none";
+     row4.style.display = "none";
+     //Paginación
+     for (let i = 0; i < pag.length; i++) {
+          //console.log(pag[i].firstChild);
+          pag[i].addEventListener('click', () => {
+               for(element of pag){
+                    if (element.classList.contains('active')) {
+                         element.classList.remove('active');
+                    }
+               }/*
+               for(element2 of rows){
+                    if (element2.classList.contains('show')) {
+                         element2.classList.remove('show');
+                    }
+               }*/
+
+               if (!pag[i].classList.contains('active')) {
+                    pag[i].classList.toggle('active');
+               }/*
+               if (!rows[i].classList.contains('show')) {
+                    rows[i].classList.toggle('show');
+               }*/
+               //row1.style.visibility = "hidden";
+               switch(i){
+                    case 0:   row1.style.display = "block"; 
+                              row2.style.display = "none";
+                              row3.style.display = "none";
+                              row4.style.display = "none";  
+                              break;  
+                    case 1:   row1.style.display = "none"; 
+                              row2.style.display = "block";
+                              row3.style.display = "none";
+                              row4.style.display = "none";  
+                              break;
+                    case 2:   row1.style.display = "none"; 
+                              row2.style.display = "none";
+                              row3.style.display = "block";
+                              row4.style.display = "none";  
+                              break;
+                    case 3:   row1.style.display = "none"; 
+                              row2.style.display = "none";
+                              row3.style.display = "none";
+                              row4.style.display = "block";  
+                              break;
+               }
+              // console.log("Pagina nro: " + pag[i]);
+          });
+          //row[i].style.visibility = "visible";
+          
+          
+     };
+     //pag[1].addEventListener("click", paginacion);
+     
 
 }
 
@@ -53,12 +114,12 @@ function leerDatosCurso(curso) {
                     return curso;
                } else {
                     return curso;
-          }
+               }
           })
           articulosCarrito = [...cursos];
      }  else {
           articulosCarrito = [...articulosCarrito, infoCurso];
-     }
+          }
 
      console.log(articulosCarrito)
 
@@ -113,5 +174,24 @@ function vaciarCarrito() {
      // forma rapida (recomendada)
      while(contenedorCarrito.firstChild) {
           contenedorCarrito.removeChild(contenedorCarrito.firstChild);
-      }
+     }
+}
+
+function paginacion(objeto){
+     //console.log(objeto.value);
+     //console.log(objeto.className);
+     if (objeto.className != "pag active") {
+          for (let index = 0; index < pag.length; index++) {
+               console.log(pag[index]);
+               //console.log(typeof(pag[index]));
+               //console.log("Prueba");
+               console.log(pag[index].classList.contains("pag active"));
+               if (pag[index].classList.contains("pag active")) {
+                    //console.log(pag[index]);
+                    pag[index].classList.toggle(" active");
+               }
+          }
+          objeto.className += " active";
+     }
+     
 }
